dd0ec0a2a996a27883a2baaead5f1798cb83a10d
ikoral <ilhankoral@outlook.com>
2020-11-05 17:25:12 +0100

    list file initilazed

---
+++ lists.py
@@ -0,0 +1,16 @@
+# square bracket
+# data collection
+
+
+# string list
+my_list = [ 'sut', 'ekmek', 'cikolata', 'princ'] #L
+
+print(my_list)
+
+my_element = my_list[0]
+
+print(my_element)
+
+#ekrana ne yazar?
+print(my_element == my_list[-4])
+
