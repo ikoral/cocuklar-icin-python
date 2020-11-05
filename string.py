a398f6012107cb20fa516564e0a9a73d79d1ddb9
ikoral <ilhankoral@outlook.com>
2020-11-05 16:39:34 +0100

    gitignore added string.py concat and new line implemented

---
+++ string.py
@@ -0,0 +1,37 @@
+# name = 'ilhan koral'
+
+# print('name:', name)
+
+# # print(name.capitalize()) ctrl + /
+
+# name_title = name.title()
+
+# name_cap = name.capitalize()
+
+# # print('new_name:', new_name)
+
+# print('name_title:' ,name_title)
+# print('name_cap:' ,name_cap)   # ctrl + space open intellisense
+
+
+# # concatanation
+
+first_name = 'Koksal'
+last_name = 'Denktas'
+the_best_friend = 'Cihangir'
+
+full_name = first_name + " " + last_name
+
+# story = full_name + " has " + the_best_friend + " as a best friend."
+
+# print(story)
+
+print('Cihangir sabah herkese "Gunaydin" dedi. ') #Escape character"
+
+print(full_name + " \nhas " + the_best_friend + "\n\tas a best friend")
+
+multiline_text = '''Merhaba
+benim adim ilhan
+Nasilsiniz?'''
+
+print(multiline_text)
