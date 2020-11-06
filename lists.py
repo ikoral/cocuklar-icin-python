a50500f6dd83145050af9bc3d49ba572378cdb98
ikoral <ilhankoral@outlook.com>
2020-11-06 20:13:08 +0100

    list shows address topic added

--- lists.py
+++ lists.py
@@ -1,16 +1,18 @@
 # square bracket
 # data collection
 
-
 # string list
 my_list = [ 'sut', 'ekmek', 'cikolata', 'princ'] #L
-
 print(my_list)
-
 my_element = my_list[0]
-
 print(my_element)
-
 #ekrana ne yazar?
 print(my_element == my_list[-4])
 
+#### list shows address in memory #####
+my_list = ["ankara","bursa","ceyhan"]
+your_list = my_list
+your_list[0] = "adana"
+new_list = my_list
+new_list = ["a","b","c"]
+print(your_list[0].upper())
