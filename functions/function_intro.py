10b595050e08a4f53e66c42ad860fb12f15183f6
ikoral <ilhankoral@outlook.com>
2020-12-20 18:41:14 +0100

    functions added, hw_func created

---
+++ functions/function_intro.py
@@ -0,0 +1,37 @@
+def greeting(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('bonjour')
+    else:
+        print('Hello')
+
+# return
+
+
+def greeting_r(lang):
+    value = ''
+    if lang == 'es':
+        value = 'Hola'
+    elif lang == 'fr':
+        value = 'bonjour'
+    else:
+        value = 'Hello'
+    return value
+
+
+test = greeting('Ilhan')
+
+test = greeting_r('Ilhan')
+
+soyle = greeting_r('fr')
+
+
+# multiple parameters
+
+def dortgen_alan(kisa_k, uzun_k):
+    alan = kisa_k * uzun_k
+    return alan
+
+
+dortgen_alan(6, 8)
---
+++ homeworks/hw_06_functions.py
@@ -0,0 +1,10 @@
+# write your pay computation function with time-and-a-half for overtime and
+# create a function called computepay which takes two parameters ( hours and  rate).
+
+# it will ask from user to enter below values.
+
+# Enter Hours: 45
+# Enter Rate: 10
+
+# And then it return total amount money to be paid as float
+# Pay: 475.0
