73d50d570187129c77adf1bb7bf79a1657614d64
ikoral <ilhankoral@outlook.com>
2021-01-23 19:52:41 +0100

    Unique characters ex added

---
+++ homeworks/H_Exercises/hw_exe_data.py
@@ -0,0 +1,5 @@
+'''Create a program that can recognize the number of unique characters in a string
+entered by the user. For example Hello should be 4 characters. And your program should
+push all unique characters to a list then print out the unique characters as new string.
+
+Wow super mom --> wo super mo'''
